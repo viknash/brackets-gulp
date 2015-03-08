21c6235ec5f2c523bd7580469c7ecc975baaeb9a
Kamil Cieślak <kamil.cieslak@outlook.com>
2015-03-08 01:07:59 +0100

    Formatted backend.js

--- backend.js
+++ backend.js
@@ -1,82 +1,83 @@
-/*global */
+/*global require, exports, console */
 
 (function () {
     'use strict';
-    
-    var _domainManager = null;
-        
+
+    var manager = null;
+
     /**
      * @private
      * Handler function for the Gulp command.
      * @param {string} tasks
      */
     function gulp(args, cwd) {
-      var exec    = require('child_process').exec,
-        fs      = require('fs'),
-        cmd     = 'gulp ' + args, 
-        child;
+        var child,
+            exec = require('child_process').exec,
+            fs = require('fs'),
+            cmd = 'gulp ' + args;
 
-      child = exec(cmd, {cwd: cwd}, function (error, stdout, stderr) {
-        if (error) {
-            console.log(error);
-            _domainManager.emitEvent('gulpDomain', 'error', [error]);
-        }
-      });
+        child = exec(cmd, {cwd: cwd}, function (error, stdout, stderr) {
+            if (error) {
+                console.log(error);
+                manager.emitEvent('gulpDomain', 'error', [error]);
+            }
+        });
 
-      child.stdout.on('data', function (data) {
-        if (args === '--tasks-simple') {
-          _domainManager.emitEvent('gulpDomain', 'tasks', [data]);
-        } else {
-          _domainManager.emitEvent('gulpDomain', 'update', [data]);
-        }
-      });
+        child.stdout.on('data', function (data) {
+            if (args === '--tasks-simple') {
+                manager.emitEvent('gulpDomain', 'tasks', [data]);
+            } else {
+                manager.emitEvent('gulpDomain', 'update', [data]);
+            }
+        });
 
-      child.stderr.on('data', function (data) {
-        console.log('err', data);
-        _domainManager.emitEvent('gulpDomain', 'error', [data]);
-      });
-      
+        child.stderr.on('data', function (data) {
+            console.log('err', data);
+            manager.emitEvent('gulpDomain', 'error', [data]);
+        });
     }
-    
+
     /**
-     * Initializes the domain 
-     * @param {DomainManager} domainManager The DomainManager for the server   
+     * Initializes the domain
+     * @param {DomainManager} domainManager The DomainManager for the server
      */
     function init(domainManager) {
-      _domainManager = domainManager;
+        manager = domainManager;
 
-      if (!_domainManager.hasDomain('gulpDomain')) {
-          _domainManager.registerDomain('gulpDomain', {major: 0, minor: 1});
-      }
-      _domainManager.registerCommand(
-        'gulpDomain',       // domain name
-        'gulp',    // command name
-        gulp,   // command handler function
-        false,          // this command is synchronous in Node
-        'Execute Gulp command',
-        [{name: 'args', type: 'string', description: 'task'},
-         {name: 'cwd',  type: 'string', description: 'cwd'}]
-      );
+        if (!manager.hasDomain('gulpDomain')) {
+            manager.registerDomain('gulpDomain', {
+                major: 0,
+                minor: 1
+            });
+        }
+        manager.registerCommand(
+            'gulpDomain', // domain name
+            'gulp', // command name
+            gulp, // command handler function
+            false, // this command is synchronous in Node
+            'Execute Gulp command',
+            [{name: 'args', type: 'string', description: 'task'},
+                {name: 'cwd', type: 'string', description: 'cwd'}]
+        );
 
-      _domainManager.registerEvent(
-        'gulpDomain',
-        'update',
-        [{name: 'data', type: 'string'}]
-      );
+        manager.registerEvent(
+            'gulpDomain',
+            'update',
+            [{name: 'data', type: 'string'}]
+        );
 
-      _domainManager.registerEvent(
-        'gulpDomain',
-        'tasks',
-        [{name: 'data', type: 'string'}]
-      );
-      
-      _domainManager.registerEvent(
-        'gulpDomain',
-        'error',
-        [{name: 'data', type: 'string'}]
-      );
+        manager.registerEvent(
+            'gulpDomain',
+            'tasks',
+            [{name: 'data', type: 'string'}]
+        );
+
+        manager.registerEvent(
+            'gulpDomain',
+            'error',
+            [{name: 'data', type: 'string'}]
+        );
     }
-    
+
     exports.init = init;
-    
 }());
